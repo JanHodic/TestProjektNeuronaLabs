ed0cc894671881e30ea4be1573e656765fa702d9
janhodic <jan.hodic@datapac.sk>
2025-12-04 00:04:57 +0100

    Authorize

--- root/backend/Neurona.Health.Api/Api/GraphQL/Queries/Query.cs
+++ root/backend/Neurona.Health.Api/Api/GraphQL/Queries/Query.cs
@@ -8,11 +8,11 @@ namespace Neurona.Health.Api.Api.GraphQL.Queries;
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1812")]
 public class Query
 {
-    //[Authorize]
+    [Authorize]
     public Task<IReadOnlyList<PatientDto>> GetPatients([Service] IPatientService patientService)
         => patientService.GetPatientsAsync();
 
-    //[Authorize]
+    [Authorize]
     public async Task<PatientDetailOutput?> GetPatientById(Guid id, [Service] IPatientService patientService)
     {
         var result = await patientService.GetPatientDetailAsync(id);
